9c3fa089c4fd0ee8f0d081bc6bebcfd7dc3b5403
Namir <nk.namirkhan1@gmail.com>
2025-07-25 16:11:23 +0530

    fix route import

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controller/AuthController");
+const authController = require("../Controller/AuthController");
 const { userAuth } = require("../Middleware/auth");
 router.post("/register", authController.SignupUser);
 router.post("/login", authController.SigninUser);
